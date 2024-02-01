2428c1c4e3ea5fbebb765d1f6418c15745927566
jamestinman <james.leeds@gmail.com>
2024-02-01 10:24:43 +0000

    Even simpler example code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,31 @@
 <script>
   import "../app.css";
-  import { page } from '$app/stores'; // Import the page store to access route-specific data.
   import Header from '$components/Header.svelte';
   import Footer from '$components/Footer.svelte';
+
+  export let data;
+  $: note = data; // Make the note reactive so that layout.svelte refreshes with url changes
+
 </script>
+
 <svelte:head>
-  {@html $page.data.head_html}
+  {@html note.head_html}
 </svelte:head>
 
 <Header />
 
 <div class="my-9">
-  <slot />
+  
+  <div class="flex flex-col gap-5 md:w-2/3 mx-auto content">
+  
+    {#if note.picture}
+      <img src={note.img.src} alt={note.img.alt} class="rounded-xl" />
+    {/if}
+  
+    {@html note.content_html}
+  
+  </div>
+
 </div>
 
 <Footer />
