9a6189985e6177f008270f50fa3c1fc7a07a04eb
jamestinman <james.leeds@gmail.com>
2024-02-01 16:59:14 +0000

    Delete +error.svelte

--- src/routes/+error.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import "../app.css";
-  import { page } from '$app/stores';
-</script>
-
-<div class="system">
-  <h1>{$page.error.message}</h1>
-  <h2>#{$page.status} Error</h2>
-</div>
