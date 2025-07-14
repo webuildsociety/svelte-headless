d71803ef51c86bb63fabb96c0bcec2ea30649525
James Leeds <james.leeds@gmail.com>
2025-07-14 12:09:00 +0100

    Make head links absolute

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <hr />
-<div class="mx-auto justify-center flex flex-row gap-10 p-5 bg-[#ace0f1] h-full text-sm">
+<div class="mx-auto justify-center flex flex-row gap-10 p-5 bg-shblue h-full text-sm">
   <p>Download the code for this blog from GitHub at 
     <a href="https://github.com/webuildsociety/svelte-headless" target="_new">
       https://github.com/webuildsociety/svelte-headless
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,30 +13,30 @@
 </script>
 
 <svelte:head>
-  <title>{note?.title ?? defaultTitle}</title>
-  <meta name="description" content={note?.description ?? defaultDescription}>
+  <title>{note?.title || defaultTitle}</title>
+  <meta name="description" content={note?.description || defaultDescription}>
   <link rel="canonical" href={note?.url}>
   
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="article">
-  <meta property="og:title" content={note?.title ?? defaultTitle}>
-  <meta property="og:description" content={note?.description ?? defaultDescription}>
+  <meta property="og:title" content={note?.title || defaultTitle}>
+  <meta property="og:description" content={note?.description || defaultDescription}>
   <meta property="og:url" content={note?.url}>
-  <meta property="og:image" content={note?.imgUrl ?? defaultImgUrl}>
+  <meta property="og:image" content={"https://svelteheadless.com" + (note?.imgUrl || defaultImgUrl)}>
 
   <!-- Twitter -->
   <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content={note?.title ?? defaultTitle}>
-  <meta name="twitter:description" content={note?.description ?? defaultDescription}>
-  <meta name="twitter:image" content={note?.imgUrl ?? defaultImgUrl}>
-  <meta name="twitter:imageAlt" content={note?.title ?? "Svelte Headless CMS"}>
+  <meta name="twitter:title" content={note?.title || defaultTitle}>
+  <meta name="twitter:description" content={note?.description || defaultDescription}>
+  <meta name="twitter:image" content={"https://svelteheadless.com" + (note?.imgUrl || defaultImgUrl)}>
+  <meta name="twitter:imageAlt" content={note?.title || "Svelte Headless CMS"}>
 
   {@html '<script type="application/ld+json">' + JSON.stringify({
     "@context": "https://schema.org/",
     "@type": "Article",
-    "headline": note?.title ?? defaultTitle,
-    "description": note?.description ?? defaultDescription,
-    "image": note?.imgUrl ?? defaultImgUrl,
+    "headline": note?.title || defaultTitle,
+    "description": note?.description || defaultDescription,
+    "image": "https://svelteheadless.com" + (note?.imgUrl || defaultImgUrl),
     "url": note?.url
   }) + '</script>'}
 
